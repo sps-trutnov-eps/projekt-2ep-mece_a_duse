6893b00a158e0948356fc8b2af8aba6049922e6d
ucenyp23 <ucenyp23@spstrutnov.cz>
2024-09-22 10:13:18 +0200

    fix imports

--- source/main_menu.py
+++ source/main_menu.py
@@ -1,18 +1,22 @@
 import pygame
+from main import Button, SCREEN_RESOLUTION
 
 def main_menu(screen: pygame.Surface) -> int:
     pygame.display.set_caption('Meče & Duše')
+
+    button_texts = ['Postava', 'Aréna', 'Trénink', 'Obchod', 'Muzeum']
+    buttons = [Button(text, SCREEN_RESOLUTION(0) // 2, SCREEN_RESOLUTION(1) // 2 + i * 60 - 150) for i, text in enumerate(button_texts)]
 
     while True:
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
-                exit(0)
+                return 9
+            for button in buttons:
+                button.handle_event(event)
 
         screen.fill(BLACK)
-        button_1 = Button('Postava', screen, SCREEN_RESOLUTION(0) // 2, SCREEN_RESOLUTION(1) // 2)
-        button_2 = Button('Aréna', screen, SCREEN_RESOLUTION(0) // 2, SCREEN_RESOLUTION(1) // 2)
-        button_3 = Button('Trénink', screen, SCREEN_RESOLUTION(0) // 2, SCREEN_RESOLUTION(1) // 2)
-        button_4 = Button('Obchod', screen, SCREEN_RESOLUTION(0) // 2, SCREEN_RESOLUTION(1) // 2)
-        button_5 = Button('Muzeum', screen, SCREEN_RESOLUTION(0) // 2, SCREEN_RESOLUTION(1) // 2)
+        for button in buttons:
+            button.draw(screen)
+        pygame.display.flip()
 
