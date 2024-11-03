65a99278df6f5333badc1f04e6c16c158c054322
ucenyp23 <ucenyp23@spstrutnov.cz>
2024-11-03 21:21:26 +0100

    exit

--- source/shop.py
+++ source/shop.py
@@ -12,7 +12,7 @@ def shop(screen: pygame.Surface) -> int:
         int: The selected level number, or 9 if the window is closed.
     """
     pygame.display.set_caption('Meče & Duše: Obchod')
-    button_texts = ['Meč', 'Štít', 'Luk', 'Brnění']
+    button_texts = ['Meč', 'Štít', 'Luk', 'Brnění', 'Exit']
     screen_width = SCREEN_RESOLUTION[0] // 2
     screen_height = SCREEN_RESOLUTION[1] // 2 - (64 * (len(button_texts) // 2))
     buttons = [
@@ -27,6 +27,8 @@ def shop(screen: pygame.Surface) -> int:
                 return 10
             for i, button in enumerate(buttons):
                 if button.handle_event(event):
+                    if i == 4:
+                        return 10
                     with open('data.txt', 'r') as filein, open('data.txt', 'r') as fileout:
                         data = filein.read().splitlines()
                         if data[i + 3] < 10 and (data[0] // (data[i + 3] ** data[i + 3])) >= 1:
